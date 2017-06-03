399e1a63acd745726806427033504c413c1a642b
Feite Brekeveld <f.brekeveld@gmail.com>
2017-06-03 14:56:00 +0000

    dist setup

--- yachain/__init__.py
+++ yachain/__init__.py
@@ -1,1 +1,6 @@
+__title__ = "yachain yaml config parser"
+__version__ = "0.1.0"
+__author__ = "Feite Brekeveld"
+__license__ = "MIT"
+__copyright__ = "Copyright 2017 Feite Brekeveld"
 from yachain.config import Config
