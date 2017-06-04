36f8e6dd88a44e675976a9e5e9c60f0fc8a927d1
hootnot <f.brekeveld@gmail.com>
2017-06-04 11:03:58 +0200

    version bumped [ci skip]

--- yachain/__init__.py
+++ yachain/__init__.py
@@ -1,5 +1,5 @@
 __title__ = "yachain yaml config parser"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Feite Brekeveld"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017 Feite Brekeveld"
