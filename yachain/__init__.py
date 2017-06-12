5a57e61a1b41dcdee45f6160758b15524b759a3b
hootnot <f.brekeveld@gmail.com>
2017-06-12 15:35:19 +0200

    pypi issues ..

--- yachain/__init__.py
+++ yachain/__init__.py
@@ -1,5 +1,5 @@
 __title__ = "yachain yaml config parser"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Feite Brekeveld"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017 Feite Brekeveld"
